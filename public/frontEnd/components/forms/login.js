8682b1729ab08ae02a6fbe4274fdbdba23c7b4b3
Scott <scottboragay06@gmail.com>
2025-09-02 17:09:43 +0800

    added: login form

---
+++ public/frontEnd/components/forms/login.js
@@ -0,0 +1,16 @@
+const username = document.querySelector(".username");
+const password = document.querySelector(".password");
+const main = document.querySelector(".main");
+
+const div = document.createElement("h1");
+div.className = "div"
+username.addEventListener("input", () => {
+    const userContent = username.value
+    const passContent = password.value;
+
+    
+
+})
+
+
+
