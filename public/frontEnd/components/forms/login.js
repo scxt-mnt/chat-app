003af078717de9586b1d4d62f3559dbae78d14ea
Scott <scottboragay06@gmail.com>
2025-09-18 19:56:51 +0800

    added: placeholder animation

--- public/frontEnd/components/forms/login.js
+++ public/frontEnd/components/forms/login.js
@@ -2,14 +2,37 @@
 
 const username = document.querySelector(".username");
 const password = document.querySelector(".password");
-const forms = document.querySelector(".forms")
+const forms = document.querySelector(".forms");
+const usernameHeader = document.querySelector(".usernamePlaceholder");
+const passwordHeader = document.querySelector(".passwordPlaceholder");
+
+username.addEventListener("focus", () => {
+    usernameHeader.style.transform = "translateY(-1.5rem)"
+});
+
+password.addEventListener("focus", () => {
+    passwordHeader.style.transform = "translateY(-1.5rem)"
+});
+
+
+username.addEventListener("blur", () => {
+    if (!username.value)
+        usernameHeader.style.transform = "translateY(0)"
+});
+
+password.addEventListener("blur", () => {
+    if (!password.value)
+        passwordHeader.style.transform = "translateY(0)"
+});
+
+
 
 forms.addEventListener("submit", async (e) => {
     e.preventDefault();
     const userContent = username.value
     const passContent = password.value;
 
-    if (userContent  && passContent) {
+    if (userContent && passContent) {
         try {
             const fetchUrl = await fetch("http://127.0.0.1:8080/login", {
                 method: "POST",
@@ -24,11 +47,11 @@ forms.addEventListener("submit", async (e) => {
             })
             const toJson = await fetchUrl.json();
             if (fetchUrl.status === 401) return console.log(toJson.msg);
-            
-            if(fetchUrl.status === 200){
-            console.log(toJson.msg)
-            window.location.href = "../index.html"
-        }
+
+            if (fetchUrl.status === 200) {
+                console.log(toJson.msg)
+                window.location.href = "../index.html"
+            }
         } catch (e) {
             console.log(e);
         }
