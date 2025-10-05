189f562db0d296452cd6e2e1e6b31b637a654918
Scott <scottboragay06@gmail.com>
2025-10-05 17:41:05 +0800

    added: image select

---
+++ public/frontEnd/components/forms/userDetails/userDetails.js
@@ -0,0 +1,8 @@
+const fileChooser  = document.querySelector(".fileSelector");
+const uploadSection  = document.querySelector(".uploadSection");
+
+uploadSection.addEventListener('click',  () => {
+    fileChooser.click();
+})
+
+
