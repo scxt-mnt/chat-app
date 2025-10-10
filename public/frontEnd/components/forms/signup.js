5c12eef9dde6176f77230ef45b8e109923f8124d
Scott <scottboragay06@gmail.com>
2025-10-10 17:36:25 +0800

    finished: responsiveness and layout

---
+++ public/frontEnd/components/forms/signup.js
@@ -0,0 +1,7 @@
+const loginDir = document.querySelector(".loginDir");
+
+
+loginDir.addEventListener("click", () => {
+    console.log("click");
+    window.location.href = "http://127.0.0.1:5501/public/frontEnd/components/forms/login.html"
+})
